474ed95599f27782c3dea1d9b2389cd1b11509c1
Andy Del Real <afdelreal04@gmail.com>
2025-08-07 13:04:42 -0500

    Working clientside typescript setup

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -7,10 +7,13 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['**/*.{js,jsx,tsx,ts}'],
     extends: [
       js.configs.recommended,
       reactHooks.configs['recommended-latest'],
+      reactHooks.configs['plugin:@typescrpt-eslint/recommended'],
+      reactHooks.configs['plugin:@typescript-eslint/recommended-type-checked'],
+      reactHooks.configs['plugin:@typescript-eslint/stylistic-type-checked'],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
